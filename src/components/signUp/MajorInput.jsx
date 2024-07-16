c358dd07a7d3aeb63b428a422ae9b2a4b07879a6
kangsuyeong <ekdh16@naver.com>
2024-07-16 14:36:44 +0900

    fix:[#1]code refactoring

--- src/components/signUp/MajorInput.jsx
+++ src/components/signUp/MajorInput.jsx
@@ -4,11 +4,13 @@ const MajorInput = ({ title, data, msg }) => {
   return (
     <div className="flex flex-col">
       <label>
-        <span className="text-[14px] font-semibold">{title}</span>
-        <span className="text-[14px] font-semibold text-[#D32F2F]"> *</span>
+        <span className="text-[14px] font-semibold">
+          {title}
+          <span className="text-[#D32F2F]"> *</span>
+        </span>
       </label>
       <select
-        className="w-[320px] h-[48px] border-solid border border-black rounded-[5px] border-[#757575] 
+        className="w-[320px] h-[48px] border-solid border rounded-[5px] border-[#757575] 
       py-[15px] px-[17px] text-[14px] text-[#757575] font-[400]"
       >
         <option disabled hidden selected>
