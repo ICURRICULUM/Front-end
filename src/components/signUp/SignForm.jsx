4b2ad773407341d1389fd27b9c1e0918b82eb94b
kangsuyeong <ekdh16@naver.com>
2024-07-23 10:51:01 +0900

    fix: dropdown 전체클릭으로 변경, form 전송추가

--- src/components/signUp/SignForm.jsx
+++ src/components/signUp/SignForm.jsx
@@ -1,10 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import NameInput from "./NameInput";
 import MajorInput from "./MajorInput";
 import Pen from "../../assets/signUp/pen.svg";
 import DropDown from "./DropDown";
 
 const SignForm = () => {
+  const [name, setName] = useState("");
+  const [SID, setSID] = useState("학번을 선택하세요.");
+  const [major, setMajor] = useState("주전공 학과를 선택하세요.");
+  const [minor, setMinor] = useState("");
+  // 예시 데이터, API로 불러올 데이터들
   const majorItems = [
     { id: 1, value: "컴퓨터공학과" },
     { id: 2, value: "정보통신공학과" },
@@ -17,30 +22,55 @@ const SignForm = () => {
     { id: 3, value: "21학번" },
   ];
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const formDate = {
+      name,
+      SID,
+      major,
+    };
+
+    console.log(formDate);
+  };
+
   return (
-    <form className="mt-[56px] mb-[83px]">
+    //로그인 폼
+    <form className="mt-[56px] mb-[83px]" onSubmit={handleSubmit}>
+      {/* 필수정보 입력 div */}
       <div className="flex mb-[36px]">
         <img src={Pen} />
         <div className="text-[20px] ml-[16px] font-semibold">필수정보 입력</div>
       </div>
+      {/* 데이터들 입력 */}
       <div>
+        {/* 이름 */}
         <div className="mb-[20px]">
-          <NameInput />
+          <NameInput onSelect={setName} />
         </div>
+        {/* 학번 */}
         <div className="mb-[20px]">
-          <DropDown title={"학번"} data={SIDItems} msg={"학번을 선택하세요."} />
+          <DropDown
+            title={"학번"}
+            data={SIDItems}
+            selectValue={SID}
+            onSelect={setSID}
+          />
         </div>
+        {/* 전공 */}
         <div className="mb-[20px]">
           <DropDown
             title={"전공"}
             data={majorItems}
-            msg={"주전공 학과를 선택하세요."}
+            selectValue={major}
+            onSelect={setMajor}
           />
         </div>
+        {/* 다중전공 */}
         <div className="mb-[38px]">
           <MajorInput title={"다중전공"} />
         </div>
       </div>
+      {/* 개인정보 및 버튼 부분 */}
       <div>
         <div className="flex mb-[13px]">
           <input className="ml-[1px]" type="checkbox" />
@@ -48,7 +78,10 @@ const SignForm = () => {
             이용약관 개인정보 수집 및 정보이용에 동의합니다.
           </div>
         </div>
-        <button className="h-[51px] w-[320px] bg-[#005BAC] text-white text-[16px] font-[600] py-[15px] px-[17px] rounded-[5px]">
+        <button
+          type="submit"
+          className="h-[51px] w-[320px] bg-[#005BAC] text-white text-[16px] font-[600] py-[15px] px-[17px] rounded-[5px]"
+        >
           가입하기
         </button>
       </div>
