c2cadcce6a0a18733be16821bda8679e2bfb1b7a
JJHyeok13 <jjinh99@naver.com>
2024-10-31 17:04:36 +0900

    feat: 충돌 제거

--- src/pages/signUpPage/signUpPage.jsx
+++ src/pages/signUpPage/signUpPage.jsx
@@ -1,13 +1,8 @@
-import React, { useState } from "react";
+import React from "react";
 import StepBox from "../../components/signUp/StepBox";
 import SignForm from "../../components/signUp/SignForm";
 
 const signUpPage = () => {
-  const [isOpen, setIsOpen] = useState(false);
-
-  const hi = () => {
-    setIsOpen(!isOpen);
-  };
   return (
     <>
       <div className="w-full mt-[84px] h-[295px] bg-[#F5F5F5] flex justify-center items-center">
