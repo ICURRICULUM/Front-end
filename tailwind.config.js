8196ff044c3db9cdaf741266cf27d0a35c03e36d
JJHyeok13 <jjinh99@naver.com>
2024-11-04 16:24:11 +0900

    FEAT: tailwind 세팅 및 시작 페이지 구현

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      boxShadow: {
+        header: "0px 2px 8px 0px #00000040",
+      },
+    },
+  },
+  plugins: [],
+};
