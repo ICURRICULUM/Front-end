113370c722d7c3fcbe3e5354069a0b06eb732a9e
JJHyeok13 <jjinh99@naver.com>
2024-11-04 16:27:18 +0900

    FEAT: 헤더 구현 및 레이아웃 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import StartPage from "./pages/startPage/startPage";
+import StartPage from "@pages/startPage/startPage";
+import Layout from "@layout/layout";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<StartPage />} />
+        <Route element={<Layout />}>
+          <Route path="/" element={<StartPage />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
---
+++ src/layout/header/header.tsx
@@ -0,0 +1,27 @@
+import { Link } from "react-router-dom";
+
+import Logo from "@assets/logo.svg";
+
+const Header = () => {
+  return (
+    <div className="fixed top-0 left-0 z-10 flex flex-row items-center justify-between w-full px-10 bg-white shadow-header">
+      <Link to="/">
+        <img src={Logo} />
+      </Link>
+
+      <div className="flex">
+        <Link to="/" className="px-6 text-xl py-7">
+          남은 강의 찾기
+        </Link>
+        <Link to="/entercourse" className="px-6 text-xl py-7">
+          수강 이력 입력
+        </Link>
+        <Link to="/" className="px-6 text-xl py-7">
+          마이페이지
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
